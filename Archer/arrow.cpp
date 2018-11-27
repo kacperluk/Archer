6305c603f281b0254d9d3100cdc185e66b589256
Kacper_Lukaszewicz <kacper.luk@o2.pl>
2018-11-27 20:48:42 +0100

    ArcherV3

--- Archer/arrow.cpp
+++ Archer/arrow.cpp
@@ -2,22 +2,49 @@
 #include <QTimer>
 #include <math.h>
 arrow::arrow(double vx,double vy):
-    Vx{vx},Vy{vy},t{0.03},g{9.81}
+    Vx{vx},Vy{vy},nextPosX{0},nextPosY{0},rotAngle{0},t{0.03},g{9.81}
+{
+    drawArrow();
+    startMoving(30);
+}
+
+void arrow::drawArrow()
 {
     this->setRect(0,0,40,10);
+}
+
+void arrow::hideArrow()
+{
+    this->setRect(0,0,0,0);
+}
+
+void arrow::startMoving(int ms)
+{
     QTimer *timer = new QTimer();
     connect(timer,SIGNAL(timeout()),this,SLOT(move()));
-    timer->start(2000);
+    timer->start(ms);
+}
+
+void arrow::rotateArrow()
+{
+    this->rotAngle=(tan(Vx/Vy)*(180/3.14))-this->rotAngle;
+    this->setRotation(rotAngle);
+    this->rotAngle=(tan(Vx/Vy)*(180/3.14));
+}
+
+void arrow::calculateNextPosValues()
+{
+    Vx=Vx;
+    Vy=Vy-(g*t);
+    nextPosX=this->x()+Vx*t;
+    nextPosY=this->y()-Vy-(g*t*t*0.5);
 }
 
 void arrow::move()
 {
-    Vx=Vx;
-    Vy=Vy-(g*t);
-    double nextPosX=this->x()+Vx*t;
-    double nextPosY=this->y()-Vy-(g*t*t*0.5);
-    rotAngle=(tan(Vx/Vy)*(180/3.14))-rotAngle;
-    this->setRotation(rotAngle);
-    rotAngle=(tan(Vx/Vy)*(180/3.14));
+    hideArrow();
+    calculateNextPosValues();
+    rotateArrow();
     this->setPos(nextPosX,nextPosY);
+    drawArrow();
     }
--- Archer/arrow.h
+++ Archer/arrow.h
@@ -10,10 +10,20 @@ class arrow:public QObject,public QGraphicsRectItem
 public:
     double Vx;
     double Vy;
+    double nextPosX;
+    double nextPosY;
     double rotAngle;
+
     const double t;
     const double g;
+
     arrow(double vx,double vy);
+    void drawArrow();
+    void hideArrow();
+    void startMoving(int ms);
+    void rotateArrow();
+    void calculateNextPosValues();
+
 public slots:
     void move();
 };
