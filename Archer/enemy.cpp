a3e0e15f3f07720579474adade4283fad978cf45
kacper.luk <kacper.luk@o2.pl>
2018-12-11 21:35:16 +0100

    Repaired version

--- Archer/enemy.cpp
+++ Archer/enemy.cpp
@@ -7,12 +7,13 @@
 #include <iostream>
 #include <Windows.h>
 #include <chrono>
-void game::mouseReleaseEvent(QMouseEvent *event)
+
+enemy::enemy()
 {
-<<<<<<< HEAD
     setRandPos();
     drawThis();
 }
+
 void enemy::drawThis()
 {
 
@@ -32,49 +33,13 @@ void enemy::setRandPos()
     int y=rand()%600;
     this->setPos(x,y);
 }
-=======
-    int vox=pressX-event->globalX();
-    int voy=-(pressY-event->globalY());
-    createArrow(vox/3,voy/3);
+
+double enemy::getEnemyX()
+{
+    return x();
 }
 
-
->>>>>>> 8382021a47fb9d7a7a40ff32300a9578e1499c96
-
-void game::mousePressEvent(QMouseEvent *event)
+double enemy::getEnemyY()
 {
-    pressX=event->globalX();
-    pressY=event->globalY();
+    return y();
 }
-void game::createScene()
-{
-    scene=new QGraphicsScene;
-    scene->setSceneRect(0,0,800,600);
-    this->setScene(scene);
-}
-void game::createArcher()
-{
-    bowman=new archer();
-    scene->addItem(bowman);
-    itemsList.push_back(bowman);
-}
-game::game()
-{
-    createScene();
-    createArcher();
-    createEnemy();
-}
-void game::createArrow(int V0x,int V0y)
-{
- arrow *arrow1=new arrow(V0x,V0y,this);
- scene->addItem(arrow1);
- itemsList.push_back(enemy1);
- arrow1->setPos(bowman->x(),bowman->y());
-}
-void game::createEnemy()
-{
-    enemy1=new enemy;
-    scene->addItem(enemy1);
-    itemsList.push_back(enemy1);
-}
-
